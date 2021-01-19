f906217ea2adb54ff979ca9253a92c6f8b843325
ezzygit <ezzaemails@gmail.com>
2021-01-19 17:31:02 +0000

    continue tomorrow

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Login from './components/Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './components/Player';
+
+const spotify = new SpotifyWebApi();
+
 
 
 function App() {
+
+ const [token, setToken] = useState(null); 
+
+useEffect(() => {
+  const hash = getTokenFromUrl();
+  window.location.hash = "";
+  const _token = hash.access_token;
+
+  if (_token) {
+    setToken(_token);
+
+    spotify.setAccessToken(_token);
+
+    spotify.getMe().then(user => {
+      console.log('person', user);
+    })    
+  }
+
+
+  console.log("I HAVE A TOKEN", token);
+
+}, []);
+
+
+
   return (
     <div className="app">
-      
-    <Login/>
-
+      {
+        token ? (
+          <Player/>
+        ) : (
+          <Login/>
+        )
+      }
     </div>
   );
 }
